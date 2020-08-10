980612406f5912ab0c22a3d4ce5b951cc91397c8
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-08-10 22:54:11 +0900

    Add custom toast animation

--- app/src/main/java/com/kwang0/customtoast/MainActivity.kt
+++ app/src/main/java/com/kwang0/customtoast/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.kwang0.customtoast
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.LinearInterpolator
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,16 +21,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         buttonToast.setOnClickListener {
-            showSystemToast()
+//            showSystemToast()
+            showCustomToast()
         }
     }
 
     private fun showSystemToast() {
         Toast.makeText(this, "$count", Toast.LENGTH_SHORT).show()
-        count++
+        addCount()
     }
 
     private fun showCustomToast() {
+        textToast.clearAnimation()
+        textToast.text = "$count"
 
+        textToast.visibility = View.VISIBLE
+        textToast.animate().apply {
+            duration = 500
+            alpha(0.8f)
+            setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    hideCustomToast()
+                }
+            })
+        }.start()
+        addCount()
+    }
+
+    private fun hideCustomToast() {
+        textToast.animate().apply {
+            duration = 500
+            startDelay = 500
+            alpha(0.0f)
+            setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    textToast.visibility = View.GONE
+                }
+            })
+        }.start()
+    }
+
+    private fun addCount() {
+        count++
     }
 }
