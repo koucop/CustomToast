bdde8bc677ee9437b96fc0b3d2c97fa9c3c8dd86
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-08-10 22:23:42 +0900

    Add show toast logic

--- app/src/main/java/com/kwang0/customtoast/MainActivity.kt
+++ app/src/main/java/com/kwang0/customtoast/MainActivity.kt
@@ -2,10 +2,20 @@ package com.kwang0.customtoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonToast.setOnClickListener {
+            Toast.makeText(this, "$count", Toast.LENGTH_SHORT).show()
+            count++
+        }
     }
 }
