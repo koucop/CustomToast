d5c86416b32aeff2dbdfcea678e8adb561d15859
kwang0 <cky1203@gmail.com>
2021-01-09 12:58:19 +0900

    Fix show short toast for gravity

--- app/src/main/kotlin/com/kwang0/customtoast/helper/SystemToastHelper.kt
+++ app/src/main/kotlin/com/kwang0/customtoast/helper/SystemToastHelper.kt
@@ -9,13 +9,29 @@ import com.kwang0.customtoast.utils.ResourceUtils
 
 object SystemToastHelper {
     fun showShortToast(context: Context, message: String?) {
-        message?.also {
-            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
-        }
+        showShortToast(context, message, 0, 0, 0)
     }
 
-    fun showShortToast(context: Context, message: String?, gravity: Gravity, xOffset: Int?, yOffset: Int?) {
-
+    fun showShortToast(
+        context: Context,
+        message: String?,
+        gravity: Int = Gravity.BOTTOM,
+        xOffset: Int = 0,
+        yOffset: Int = 0
+    ) {
+        message?.also {
+            when {
+                gravity == 0 && xOffset == 0 && yOffset == 0 -> {
+                    Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(context, it, Toast.LENGTH_SHORT)?.also { toast ->
+                        toast.setGravity(gravity, xOffset, yOffset)
+                        toast.show()
+                    }
+                }
+            }
+        }
     }
 
     fun showLongToast(context: Context, message: String?) {
@@ -25,7 +41,7 @@ object SystemToastHelper {
     }
 
     fun showLongToast(context: Context, message: String?, gravity: Gravity, xOffset: Int?, yOffset: Int?) {
-        
+
     }
 
     fun showToast(view: View, message: String?) {
